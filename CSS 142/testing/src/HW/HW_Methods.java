efc0e9826dc2cb979ac8a7af5319979b0ba6e368
A_ux5311 <trinhminh02@gmail.com>
2022-01-14 20:37:40 -0800

    Add files via upload

---
+++ CSS 142/testing/src/HW/HW_Methods.java
@@ -0,0 +1,145 @@
+package HW;
+
+public class HW_Methods {
+    public static void main(String[] args) {
+        runProgramA();
+
+    }
+
+    public static void runProgramA() {
+        String platform = "||";
+        String platQuestion = "|?|";
+        //String coin = "@";
+
+        System.out.println("Mario Board 1:");
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + "                                          //Segment 1");
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.printf("%n");
+
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + "       " + platQuestion);
+        System.out.println(platform);
+        System.out.println(platform + "                                          //Segment 2");
+        System.out.println(platform + "       " + platQuestion);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.printf("%n");
+
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + platform);
+        System.out.println(platform + platform + platform + "                                        //Segment 3");
+        System.out.println(platform + platform + platform + platform);
+        System.out.println(platform + platform + platform + platform + platform);
+        System.out.printf("%n");
+        System.out.printf("%n");
+
+        System.out.println(platform + platform + platform + platform +platform);
+        System.out.println(platform + platform + platform + platform);
+        System.out.println(platform + platform + platform + "                                        //Segment 3 repeat");
+        System.out.println(platform + platform);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.printf("%n");
+
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + "       " + platQuestion);
+        System.out.println(platform);
+        System.out.println(platform + "                                            //Segment 2");
+        System.out.println(platform + "       " + platQuestion);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.printf("%n");
+
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + platform);
+        System.out.println(platform + platform + platform +"                                          //Segment 4");
+        System.out.println(platform + platform + platform + platform);
+        System.out.println(platform + platform + platform + platform + platform);
+        System.out.println(platform + platform + platform + platform + platform + platform);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + "            ^");
+        System.out.println(platform + "|------------");
+        System.out.printf("%n");
+
+        System.out.printf("%n");
+        System.out.printf("%n");
+        System.out.printf("%n");
+        System.out.printf("%n");
+        System.out.printf("%n");
+        System.out.printf("%n");
+        System.out.printf("%n");
+
+        System.out.println("Mario board 2:");
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + "                                          //Segment 1");
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.printf("%n");
+
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + platform);
+        System.out.println(platform + platform + platform +"                                       //Segment 2");
+        System.out.println(platform + platform + platform + platform);
+        System.out.println(platform + platform + platform + platform + platform);
+        System.out.printf("%n");
+
+        System.out.println(platform + platform + platform + platform +platform);
+        System.out.println(platform + platform + platform + platform);
+        System.out.println(platform + platform + platform + "                                       //Segment 3");
+        System.out.println(platform + platform);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.printf("%n");
+
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + "       " + platQuestion);
+        System.out.println(platform);
+        System.out.println(platform + "                                          //Segment 4");
+        System.out.println(platform + "       " + platQuestion);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.printf("%n");
+
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + "       " + platQuestion);
+        System.out.println(platform);
+        System.out.println(platform + "                                          //Segment 4 repeat");
+        System.out.println(platform + "       " + platQuestion);
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.printf("%n");
+
+        System.out.println(platform);
+        System.out.println(platform);
+        System.out.println(platform + platform);
+        System.out.println(platform);
+        System.out.println(platform + platform + platform + "                                       //Segment 2 repeat");
+        System.out.println(platform);
+        System.out.println(platform + platform + platform + platform);
+        System.out.println(platform);
+        System.out.println(platform + platform + platform + platform + platform);
+        System.out.println(platform);
+        System.out.println(platform + platform + platform + platform + platform);
+        System.out.println(platform + platform + platform + platform + platform);
+        System.out.println(platform + platform + platform + platform + platform);
+        System.out.println(platform + platform + platform + platform + platform + "                      ^");
+        System.out.println(platform + platform + platform + platform + platform + "-----------------------");
+    }
+
+    
+}
